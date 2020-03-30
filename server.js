d042bda28f7f125c9268a453c01c7c00e8223c26
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-03-31 00:45:55 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use (express.static(path.join(__dirname, 'public')));
+
+//Run when client connects
+io.on('connection', socket => {
+    
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatCord!'); // emit just to the single user that is conncetingsddd
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat'); //will emit to everybody except the user to the user that's connecting
+
+    //Run when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
+})
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
